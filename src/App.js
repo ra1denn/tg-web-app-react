bdcd01cec29603da2086d8f0292b0c5f93a9e141
Maria Ra1den <109177026+ra1denn@users.noreply.github.com>
2025-05-17 13:31:54 +0300

    onClick

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
   return (
     <div className="App">
       work
-      <button onClick={onClick}>закрыть</button>
+      <button onClick={onClose}>закрыть</button>
     </div>
   );
 }
