633a4cb5501fdfacff4cbf831990c1086d0e4d9f
Maria Ra1den <109177026+ra1denn@users.noreply.github.com>
2025-05-17 16:53:03 +0300

    text

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -38,7 +38,7 @@ const ProductList = () => {
         } else {
             tg.MainButton.show();
             tg.MainButton.setParams({
-                text: 'Купить ${getTotalPrice(newItems)}'
+                text: `Купить ${getTotalPrice(newItems)}`
             })
         }
     }
