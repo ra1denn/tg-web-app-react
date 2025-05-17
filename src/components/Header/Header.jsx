0f79efa953b2d7d02c06916e6ac2b4e036745a95
Maria Ra1den <109177026+ra1denn@users.noreply.github.com>
2025-05-17 13:49:24 +0300

    username

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import Button from '../Button/Button';
+
+const Header = () => {
+    const tg = window.Telegram.WebApp;
+    const onClose = () => {
+        tg.close()
+    }
+
+    return (
+        <div>
+            <Button onClick={onClose}>закрыть</Button>
+            <span className={'username'}>
+                {tg.initDataUnsafe?.user?.username}
+            </span>
+        </div>
+    );
+};
+
+export default Header;
